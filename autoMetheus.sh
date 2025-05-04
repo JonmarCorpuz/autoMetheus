27a221bf5bdca7a09bae8778fe5139d146689c73
Jon <121370426+JonmarCorpuz@users.noreply.github.com>
2025-05-04 19:24:52 -0400

    Update autoMetheus.sh

--- autoMetheus.sh
+++ autoMetheus.sh
@@ -6,68 +6,97 @@ source staticVariables.env
 # Change file permissions
 sudo chmod +x ./Scripts/*
 
-if ! sshpass --version &> /dev/null;
-then  
-  echo -e "${YELLOW}[NOTICE]${WHITE} Installing sshpass.]"
-  sudo apt -y install sshpass
-fi 
-
-# ==== USER INPUT ===================================================
 while true;
-do
+do 
+    # ==== OPTIONS MENU ==============================================================================================
+    echo """
+    Available options:
+    [1] Install Prometheus
+    [2] Tweak Prometheus
+    [3] Install Exporters
+    [4] Query Metrics
+    """
+    read -p "Please enter the option for the operation that you would like to accomplish: " option
 
-  # Target server
-  read -p "Enter the IP address of the server that you want to install Prometheus on (Enter 127.0.0.1 for localhost): " targetDestination
-  
-  # Target server address is valid
-  if $targetDestination =~ $IPv4;
-  then
-
-    # Server is reachable
-    if ping -c 4 $targetDestination &> /dev/null; 
+    if $option == "1";
     then 
-      
-      # 
-      if $targetDestination != "127.0.0.1";
-      then
-        
+        # ==== INSTALL PROMETHEUS ========================================================================================
         while true;
-        do 
-          # Prompt for user
-          read -p "Please provide the user on the remote server that'll run the script: " remoteUser
+        do
 
-          # Prompt for password
-          read -p "Please enter the password for the user on the remote server that'll run the script: " remoteUserPassword
+        # Target server
+        read -p "Enter the IP address of the server that you want to install Prometheus on (Enter 127.0.0.1 for localhost): " targetDestination
+        
+        # Target server address is valid
+        if $targetDestination =~ $IPv4;
+        then
 
-          # Test SSH
-          if sshpass -p $remoteUserPassword ssh StrictHostKeyChecking=no $remoteUser@$targetDestination;
-          then
+            # Server is reachable
+            if ping -c 4 $targetDestination &> /dev/null; 
+            then 
+            
+            # 
+            if $targetDestination != "127.0.0.1";
+            then
+                
+                while true;
+                do 
+                    # Prompt for user
+                    read -p "Please provide the user on the remote server that'll run the script: " remoteUser
 
-            # Copy all dependencies to remote host
-            if scp * $remoteUser@$targetDestination:/home/$remoteUser;
-            then  
-              sshpass -p $remoteUserPassword ssh StrictHostKeyChecking=no $remoteUser@$targetDestination "chmod +x "
-              break
-            fi 
-            echo -e "${RED}[ERROR]${WHITE} Unable to copy files to remote host. Please enter a valid username and password."
+                    # Prompt for password
+                    read -p "Please enter the password for the user on the remote server that'll run the script: " remoteUserPassword
 
-          fi
-          echo -e "${RED}[ERROR]${WHITE} Unable to SSH to the remote server."
+                    # Test SSH
+                    if sshpass -p $remoteUserPassword ssh StrictHostKeyChecking=no $remoteUser@$targetDestination;
+                    then
 
-        done 
-        break
-      else
+                        # Copy all dependencies to remote host
+                        if scp * $remoteUser@$targetDestination:/home/$remoteUser;
+                        then  
+                           sshpass -p $remoteUserPassword ssh StrictHostKeyChecking=no $remoteUser@$targetDestination "chmod +x "
+                            break
+                    fi 
+                    echo -e "${RED}[ERROR]${WHITE} Unable to copy files to remote host. Please enter a valid username and password."
 
-        # Install Prometheus locally
-        if ! sudo systemctl status prometheus.service &> /dev/null;
-        then 
-          echo -e "${YELLOW}[NOTICE]${WHITE} Installing Prometheus."
-          ./Scripts/install-Prometheus.sh
+                fi
+                echo -e "${RED}[ERROR]${WHITE} Unable to SSH to the remote server."
+
+                done 
+                break
+            else
+
+                # Install Prometheus locally
+                if ! sudo systemctl status prometheus.service &> /dev/null;
+                then 
+                    echo -e "${YELLOW}[NOTICE]${WHITE} Installing Prometheus."
+                    ./Scripts/install-Prometheus.sh
+                fi 
+            fi
+            echo -e "${RED}[ERROR]${WHITE}"
+            fi
+            echo -e "${RED}[ERROR]${WHITE}"
         fi 
-      fi
-      echo -e "${RED}[ERROR]${WHITE}"
-    fi
-    echo -e "${RED}[ERROR]${WHITE}"
-  fi 
-  echo -e "${RED}[ERROR]${WHITE} Please enter a valid IPv4 address"
-done
+        echo -e "${RED}[ERROR]${WHITE} Please enter a valid IPv4 address"
+        done
+
+        echo -e "${GREEN}[SUCCESS]${WHITE} Prometheus was successfully installed and running on $targetDestination:9090."
+
+
+    # ==== PROMETHEUS CONFIGURATION ==================================================================================
+    elif $option == "2";
+    then
+
+    # ==== EXPORTERS =================================================================================================
+    elif $option == "3";
+    then
+
+    # ==== PROMQL QUERIES ============================================================================================
+    elif $option =="4";
+    then 
+        python3 ./Scripts/queryPromQL.py
+    else 
+        echo -e "${RED}[ERROR]${WHITE} $option is not a valid option. Please enter a valid option to continue."
+    fi 
+
+done 
