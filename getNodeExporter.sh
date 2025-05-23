c9258e1aa9906071eb2c51b4d637078f849d8ba2
Jon <121370426+JonmarCorpuz@users.noreply.github.com>
2025-05-23 17:09:49 -0400

    Create getNodeExporter.sh

---
+++ getNodeExporter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ==== STATIC VARIABLES ==================================================================================
+WHITE="\033[0m"
+RED="\033[0;31m"
+YELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+
+INTEGER='^[0-9]+$'
+IPv4='^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$'
+
+# ==== MAIN BODY =========================================================================================
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+tar xvfz node_exporter-1.9.1.linux-amd64.tar.gz
+cd node_exporter-1.9.1.linux-amd64
+./node_exporter
+
+# curl http://localhost:9100/metrics
