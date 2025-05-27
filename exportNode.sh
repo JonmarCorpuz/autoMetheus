8903dbc87a9936b4b7168d7111646f7927b672ad
Jon <121370426+JonmarCorpuz@users.noreply.github.com>
2025-05-26 21:11:13 -0400

    Update exportNode.sh

--- exportNode.sh
+++ exportNode.sh
@@ -6,17 +6,45 @@ RED="\033[0;31m"
 YELLOW="\033[1;33m"
 GREEN="\033[0;32m"
 
-INTEGER='^[0-9]+$'
 IPv4='^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$'
 
 # ==== MAIN BODY =========================================================================================
 
+sudo su -c 'echo """
+  - job_name: 'node_exporter'
+    static_configs:
+""" >> /etc/prometheus/prometheus.yaml'
+
+while true;
+do
+
+  read -p "Enter the IP address for the Linux node that you would like to scrape: " deviceIP
+
+  if [[ $deviceIP =~ $IPv4 ]];
+  then
+    sudo su -c 'echo "       - $deviceIP" >> /etc/prometheus/prometheus.yaml'
+
+    read -p "Would you like to add another device? [Y] or [N] " addDevice
+    if [[ $addDevice =~ "N" ]];
+    then
+      break
+    fi
+    echo ""
+  else
+    echo -e "${RED}[ERROR]${WHITE} $deviceIP is not a valid IPv4 address\n"
+  fi
+
+done
+
 echo -e "${YELLOW}[WARNING]${WHITE} Installing the Node Exporter\n"
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
-tar xvfz node_exporter-1.9.1.linux-amd64.tar.gz
+tar xvfz node_exporter-1.9.1.linux-amd64.tar.gz &> /dev/null
 cd node_exporter-1.9.1.linux-amd64
 ./node_exporter &
+
+processID=$(ps -ef | grep snmp_exporter | grep -v grep | awk '{print $2}')
+echo -e "\n${GREEN}[SUCCESS]${WHITE} The Node Exporter was successfully installed and is successfully running in the background under process $processID on this host"
 
 echo -e "\n${GREEN}[SUCCESS]${WHITE} Node Exporter was successfully installed"
 
