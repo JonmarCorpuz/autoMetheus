dafd413c3191055d198412c3c5462dbbf3a48665
Jon <121370426+JonmarCorpuz@users.noreply.github.com>
2025-05-27 22:53:49 -0400

    Update exportNode.sh

--- exportNode.sh
+++ exportNode.sh
@@ -10,49 +10,24 @@ IPv4='^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[
 
 # ==== MAIN BODY =========================================================================================
 
-touch tmp.yaml
-
-#while true;
-#do
-
-#  read -p "Enter the IP address for your Prometheus server: " prometheusIP
-
-#  if ! ping $prometheusIP -c 4 &> /dev/null;
-#  then
-#    echo -e "${RED}[ERROR]${WHITE} $prometheusIP was unreachable"
-#  else
-#    read -p "Enter SSH username: " sshUser
-#    read -p "Enter SSH password: " sshPassword
-
-#    test=$(sshpass -p '$sshPassword' ssh -o StrictHostKeyChecking=no '$sshUser@$prometheusIP')
-#    if $test;
-#    then
-#      sshpass -p "$sshPassword" ssh -o StrictHostKeyChecking=no "$sshUser@$prometheusIP" \
-#      'echo \"- job_name: node_exporter\n  static_configs:\n    - targets: [localhost:9100]\" | sudo tee -a /etc/prometheus/prometheus.yaml'
-#    else
-#      echo -e "${RED}[ERROR]${WHITE} ok"
-#    fi
-#  fi
-#done
-
-echo """
-  - job_name: node_exporter
-    static_configs:
-""" >> ./tmp.yaml
+touch appendToPrometheus.yaml
+echo -e "  - job_name: node_exporter\n    static_configs:      - targets: """ >> ./appendToPrometheus.yaml
 
 while true;
 do
 
   read -p "Enter the IP address for the Linux node that you would like to scrape: " deviceIP
+  echo ""
 
   if [[ $deviceIP =~ $IPv4 ]];
   then
-    cat $deviceIP
-    echo "       - ${deviceIP}" >> ./tmp.yaml
+    echo $deviceIP
+    echo "       - ${deviceIP}" >> ./appendToPrometheus.yaml
 
     read -p "Would you like to add another device? [Y] or [N] " addDevice
     if [[ $addDevice =~ "N" ]];
     then
+      echo " " >> ./appendToPrometheus.yaml
       break
     fi
     echo ""
@@ -61,16 +36,39 @@ do
   fi
 done
 
-echo -e "${YELLOW}[WARNING]${WHITE} Installing the Node Exporter\n"
+while true;
+do
 
+  read -p "Enter the IP address for your Prometheus server: " prometheusIP
+
+  if ping $prometheusIP -c 4 &> /dev/null;
+  then
+
+    read -p "Enter SSH username: " sshUser
+    #read -p "Enter SSH password: " sshPassword
+
+    if scp appendToPrometheus.yaml $sshUser@$prometheusIP:~/;
+    then
+      echo -e "${YELLOW}[NOTICE]${WHITE} appendToPrometheus.yaml was copied to the home directory of $sshUser on $prometheusIP"
+      echo -e "${YELLOW}[NOTICE]${WHITE} Please append the contents of this file to your prometheus.yaml configuration file"
+    else
+      echo -e "${RED}[ERROR]${WHITE} Failed to copy appendToPrometheus.yaml to the home directory of $sshUser on $prometheusIP"
+      echo -e "${YELLOW}[NOTICE]${WHITE} Please append the contents of this file to your prometheus.yaml configuration file"
+    fi
+  else
+    echo -e "${RED}[ERROR]${WHITE} $prometheusIP was unreachable"
+    echo -e "${YELLOW}[NOTICE]${WHITE} Please append the contents of this file to your prometheus.yaml configuration file"
+  fi
+done
+
+echo -e "${YELLOW}[WARNING]${WHITE} Installing the Node Exporter\n"
 wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
 tar xvfz node_exporter-1.9.1.linux-amd64.tar.gz &> /dev/null
 cd node_exporter-1.9.1.linux-amd64
-./node_exporter &
+./node_exporter & 
+sleep 10
 
 processID=$(ps -ef | grep node_exporter | grep -v grep | awk '{print $2}')
 echo -e "\n${GREEN}[SUCCESS]${WHITE} The Node Exporter was successfully installed and is successfully running in the background under process $processID on this host"
-
-echo -e "\n${GREEN}[SUCCESS]${WHITE} Node Exporter was successfully installed"
 
 # curl http://localhost:9100/metrics
