b7cb1afeb388dc240e60b5c3ddafa5dc4789ec1a
Jon <121370426+JonmarCorpuz@users.noreply.github.com>
2025-05-29 21:47:34 -0400

    Update exportSNMP.sh

--- exportSNMP.sh
+++ exportSNMP.sh
@@ -8,8 +8,9 @@ GREEN="\033[0;32m"
 
 IPv4='^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])$'
 
-# ==== SNMP.YAML =========================================================================================
+# ==== MAIN BODY =========================================================================================
 
+# Specify what to add to the Prometheus server's configuration file
 touch tmp.yaml
 echo -e "  - job_name: snmp\n    static_configs:\n      - targets:" >> ./tmp.yaml
 
@@ -52,13 +53,13 @@ echo """
       - targets: [localhost:9116]
 """ >> ./tmp.yaml
 
-# ==== MAIN BODY =========================================================================================
-
+# Install the SNMP Exporter
 echo -e "${YELLOW}[WARNING]${WHITE} Installing the SNMP Exporter\n"
 wget https://github.com/prometheus/snmp_exporter/releases/download/v0.29.0/snmp_exporter-0.29.0.linux-amd64.tar.gz
 tar xzvf snmp_exporter-0.29.0.linux-amd64.tar.gz &> /dev/null
 cd snmp_exporter-0.29.0.linux-amd64
 
+# Create the required snmp.yaml file
 echo """
 auths:
   public_v2:
@@ -71,6 +72,7 @@ modules:
     - 1.3.6.1.4.1.318.1.1.1.2
 """ > snmp.yaml
 
+# Run the SNMP Exporter in the background
 echo -e "${YELLOW}[INFO]${WHITE} Running the SNMP Exporter on the localhost"
 ./snmp_exporter --config.file=./snmp.yaml &
 sleep 5
@@ -78,6 +80,7 @@ sleep 5
 processID=$(ps -ef | grep snmp_exporter | grep -v grep | awk '{print $2}')
 echo -e "\n${GREEN}[SUCCESS]${WHITE} The SNMP Exporter was successfully installed and is successfully running in the background under process $processID on this host"
 
+# Cleanup
 echo -e "\nPlease add the following to your prometheus configuration file:\n"
 cat ../tmp.yaml && echo ""
 rm ../tmp.yaml
